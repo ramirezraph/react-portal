076cae226638128cabdceb9697ba1c6c81ad01aa
Raphael Ramirez <>
2022-03-12 00:03:28 +0800

    added prettier-plugin-tailwindcss

--- src/app/pages/Landing/index.tsx
+++ src/app/pages/Landing/index.tsx
@@ -9,19 +9,19 @@ export function Landing() {
       <Helmet>
         <title>Landing</title>
       </Helmet>
-      <div className="w-screen h-screen flex items-center space-x-10 p-6">
-        <div className="flex-1 flex justify-end ">
+      <div className="flex h-screen w-screen items-center space-x-10 p-6">
+        <div className="flex flex-1 justify-end ">
           <img src={image} className="object-cover" alt="Undraw" width={600} />
         </div>
-        <div className="flex-1 h-full flex flex-col justify-center">
-          <span className="text-xl leading-loose select-none text-zinc-800">
+        <div className="flex h-full flex-1 flex-col justify-center">
+          <span className="select-none text-xl leading-loose text-zinc-800">
             welcome to
           </span>
-          <h1 className="text-6xl select-none">DPVMHS Portal</h1>
-          <span className="text-xl leading-loose select-none text-zinc-800">
+          <h1 className="select-none text-6xl">DPVMHS Portal</h1>
+          <span className="select-none text-xl leading-loose text-zinc-800">
             Dr. Pablito V. Mendoza Sr. High School
           </span>
-          <button className="bg-primary w-fit py-2 px-24 text-white rounded-sm mt-24">
+          <button className="mt-24 w-fit rounded-sm bg-primary py-2 px-24 text-white">
             Get Started
           </button>
         </div>
