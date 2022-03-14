0fa2f1fc9c0a202d7f76cd7f860a24c5b2bdff3c
Raphael Ramirez <>
2022-03-14 20:38:00 +0800

    Revert "Merge pull request #4 from ramirezraph/feature/main"
    This reverts commit 19a391285ae3798361211992e3c645aefa949803, reversing
    changes made to 0450a06e757adfe417153bfd12e5a0babbf6ccaa.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/@vechaiui/**/*.{js,ts,jsx,tsx}',
-  ],
-  darkMode: 'class', // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      // colors: {
-      //   document: '#F2F2F2',
-      //   primary: '#6C63FF',
-      //   secondary: '#FFFFFF',
-      // },
+      colors: {
+        document: '#F2F2F2',
+        primary: '#6C63FF',
+        secondary: '#FFFFFF',
+      },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@vechaiui/core')],
+  plugins: [],
 };
