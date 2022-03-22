b61a109112b462467c5d5027f1adad01a745cc8f
Raphael Ramirez <>
2022-03-22 23:07:22 +0800

    drawer and links

---
+++ src/app/components/Navbar/components/navButton.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Button, Group, Text, UnstyledButton } from '@mantine/core';
+import { Icon, IconProps } from 'tabler-icons-react';
+import { Link } from 'react-router-dom';
+
+interface Props {
+  to: string;
+  icon: IconProps;
+  text: string;
+}
+
+export function NavButton(props: Props) {
+  const { icon, to, text } = props;
+
+  return (
+    <UnstyledButton className="w-full py-1">
+      <Link className="text-black no-underline" to={to}>
+        <Group>
+          {icon}
+          <Text>{text}</Text>
+        </Group>
+      </Link>
+    </UnstyledButton>
+  );
+}
--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
-import { Navbar, Text } from '@mantine/core';
+import {
+  Avatar,
+  Divider,
+  Group,
+  Navbar,
+  Text,
+  useMantineTheme,
+} from '@mantine/core';
+import {
+  Book,
+  Calendar,
+  ChartArrowsVertical,
+  Home,
+  Message,
+  Users,
+} from 'tabler-icons-react';
+import { NavButton } from './components/navButton';
 
 interface Props {
   hidden: boolean;
 }
 
 export function AppNavbar(props: Props) {
+  const theme = useMantineTheme();
   const { hidden } = props;
 
   return (
@@ -18,9 +35,48 @@ export function AppNavbar(props: Props) {
       // when viewport size is less than theme.breakpoints.sm navbar width is 100%
       // viewport size > theme.breakpoints.sm – width is 300px
       // viewport size > theme.breakpoints.lg – width is 400px
-      width={{ sm: 300, lg: 400 }}
+      width={{ sm: 200, lg: 230 }}
+      className="bg-white p-6"
     >
-      <Text>Links</Text>
+      <Group direction="column">
+        <Group direction="column">
+          <Avatar
+            size={'lg'}
+            src={null}
+            radius="lg"
+            alt="John D. Doe"
+            color={theme.primaryColor}
+          >
+            JD
+          </Avatar>
+          <Text weight={'bold'}>John D. Doe</Text>
+        </Group>
+        <Divider
+          variant="solid"
+          size="sm"
+          color={'dark'}
+          className="my-2 w-8"
+        />
+        <NavButton to="/" text="Dashboard" icon={<Home />} />
+        <NavButton to="/discussions" text="Discussions" icon={<Message />} />
+        <NavButton to="/classes" text="Classes" icon={<Users />} />
+        <NavButton
+          to="/discussions"
+          text="Grades"
+          icon={<ChartArrowsVertical />}
+        />
+        <NavButton to="/calendar" text="Calendar" icon={<Calendar />} />
+        <Divider
+          variant="solid"
+          size="sm"
+          color={'dark'}
+          className="my-2 w-8"
+        />
+        <NavButton to="/classes" text="RLW 101" icon={<Book />} />
+        <NavButton to="/classes" text="PE 4" icon={<Book />} />
+        <NavButton to="/classes" text="CPE 401" icon={<Book />} />
+        <NavButton to="/classes" text="CPE 402" icon={<Book />} />
+      </Group>
     </Navbar>
   );
 }
