d445e7931bb6478cab618575b70c87f85edb1ca8
Raphael Ramirez <ramirez.raph@yahoo.com>
2022-03-24 21:53:21 +0800

    core layout

--- src/app/components/PageContainer/index.tsx
+++ src/app/components/PageContainer/index.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 
 interface Prop {
   children?: React.ReactNode;
+  className?: string;
 }
 
 export function PageContainer(props: Prop) {
-  return <div className="p-6">{props.children}</div>;
+  return <div className={`p-6 ${props.className}`}>{props.children}</div>;
 }
--- src/app/pages/Dashboard/index.tsx
+++ src/app/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
-import { Container } from '@mantine/core';
+import { Button, Container, Group, Text } from '@mantine/core';
 import { PageContainer } from 'app/components/PageContainer/Loadable';
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Eye, EyeOff } from 'tabler-icons-react';
 
 export function Dashboard() {
   return (
@@ -9,8 +10,34 @@ export function Dashboard() {
       <Helmet>
         <title>Dashboard</title>
       </Helmet>
-      <PageContainer>
-        <span className="text-lg">Dashboard</span>
+      <PageContainer className="h-full p-0">
+        <Group
+          direction="row"
+          align={'baseline'}
+          noWrap
+          className="h-full w-full"
+        >
+          <div className="w-2/3 p-6">
+            <Text size="lg" weight={'bold'}>
+              Dashboard
+            </Text>
+          </div>
+          <div className="h-full w-1/3 bg-white p-6">
+            <Group noWrap position="apart">
+              <Text size="xl" className="font-semibold">
+                Calendar
+              </Text>
+              <Button
+                size="sm"
+                leftIcon={<EyeOff size={16} />}
+                color={'gray'}
+                variant="subtle"
+              >
+                Hide
+              </Button>
+            </Group>
+          </div>
+        </Group>
       </PageContainer>
     </>
   );
