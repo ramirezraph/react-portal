8a382b0864a19e8354d5609cdccc87d11f165135
Raphael Ramirez <ramirez.raph@yahoo.com>
2022-04-04 13:35:38 +0800

    post component

--- src/app/pages/Class/components/ClassTabs/DiscussionTab.tsx
+++ src/app/pages/Class/components/ClassTabs/DiscussionTab.tsx
@@ -1,5 +1,7 @@
-import { Text } from '@mantine/core';
+import { ActionIcon, Avatar, Card, Group, TextInput } from '@mantine/core';
+import { Post } from 'app/components/Post/Loadable';
 import * as React from 'react';
+import { At, File, Photo } from 'tabler-icons-react';
 
 interface Props {
   // someProps: string
@@ -9,8 +11,33 @@ export function DiscussionTab(props: Props) {
   // const { someProps } = props;
 
   return (
-    <div className="p-6">
-      <Text>Discussion Tab</Text>
+    <div className="bg-transparent py-3">
+      <Card>
+        <Group noWrap className="rounded-md">
+          <Avatar color={'primary'} radius="xl">
+            JD
+          </Avatar>
+          <TextInput
+            placeholder="Write something for the class"
+            variant="filled"
+            radius="xl"
+            required
+            className="flex-grow"
+          />
+          <ActionIcon>
+            <Photo />
+          </ActionIcon>
+          <ActionIcon>
+            <File />
+          </ActionIcon>
+          <ActionIcon>
+            <At />
+          </ActionIcon>
+        </Group>
+      </Card>
+      <div>
+        <Post />
+      </div>
     </div>
   );
 }
