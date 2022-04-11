a5c10849a800d165eeb2f85ec6c13f21dfa72932
Raphael Ramirez <ramirez.raph@yahoo.com>
2022-04-11 14:01:16 +0800

    redesign control
    + added tooltips
    + post attach file number

--- src/app/pages/Class/components/ClassAccordionControl/index.tsx
+++ src/app/pages/Class/components/ClassAccordionControl/index.tsx
@@ -1,10 +1,17 @@
-import { Group, ActionIcon, Switch, Button, Tooltip } from '@mantine/core';
-import { useToggle } from '@mantine/hooks';
+import { Group, ActionIcon, Button, Tooltip, Text } from '@mantine/core';
 import { LiveSwitch } from 'app/components/LiveSwitch/Loadable';
 import * as React from 'react';
 import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { FileUpload, Pencil, SquarePlus, Trash } from 'tabler-icons-react';
+import {
+  FileUpload,
+  Maximize,
+  Message,
+  Pencil,
+  SquarePlus,
+  ThumbUp,
+  Trash,
+} from 'tabler-icons-react';
 import { useClassroomSlice } from '../../slice';
 import { ClassAccordionType } from '../ClassUnitAccordion';
 
@@ -20,16 +27,6 @@ export function ClassAccordionControl(props: Props) {
 
   const navigate = useNavigate();
   let location = useLocation();
-
-  const [switchLabel, toggleSwitchLabel] = useToggle(null, ['Live', 'Draft']);
-
-  React.useEffect(() => {
-    if (live) {
-      toggleSwitchLabel('Live');
-    } else {
-      toggleSwitchLabel('Draft');
-    }
-  }, [live, toggleSwitchLabel]);
 
   const { actions } = useClassroomSlice();
   const dispatch = useDispatch();
@@ -66,33 +63,52 @@ export function ClassAccordionControl(props: Props) {
           </Button>
         )}
         {type === ClassAccordionType.Lesson && (
-          <ActionIcon variant="transparent">
-            <FileUpload />
-          </ActionIcon>
+          <Tooltip label="Attach a file" position="bottom" withArrow>
+            <ActionIcon variant="transparent">
+              <FileUpload />
+            </ActionIcon>
+          </Tooltip>
         )}
 
         <LiveSwitch live={live} onToggle={toggleSwitch} />
       </Group>
-      <Group className="gap-0" noWrap>
-        <Tooltip label="Edit" position="bottom" withArrow>
-          <ActionIcon
-            variant="transparent"
-            onClick={
-              type === ClassAccordionType.Lesson
-                ? displayLessonModalOnEdit
-                : undefined
-            }
-          >
-            <Pencil />
-          </ActionIcon>
-        </Tooltip>
+      {type === ClassAccordionType.Unit && (
+        <Group className="gap-0" noWrap>
+          <Tooltip label="Edit" position="bottom" withArrow>
+            <ActionIcon variant="transparent">
+              <Pencil />
+            </ActionIcon>
+          </Tooltip>
 
-        <Tooltip label="Delete" position="bottom" withArrow>
-          <ActionIcon color={'red'} variant="transparent">
-            <Trash />
-          </ActionIcon>
-        </Tooltip>
-      </Group>
+          <Tooltip label="Delete" position="bottom" withArrow>
+            <ActionIcon color={'red'} variant="transparent">
+              <Trash />
+            </ActionIcon>
+          </Tooltip>
+        </Group>
+      )}
+      {type === ClassAccordionType.Lesson && (
+        <Group spacing={'xs'}>
+          <Button
+            variant="subtle"
+            compact
+            color={'dark'}
+            className="px-0"
+            onClick={displayLessonModalOnEdit}
+          >
+            <Message />
+            <Text className="ml-2">1</Text>
+          </Button>
+          <Tooltip label="Lesson View" position="bottom" withArrow>
+            <ActionIcon
+              variant="transparent"
+              onClick={displayLessonModalOnEdit}
+            >
+              <Maximize />
+            </ActionIcon>
+          </Tooltip>
+        </Group>
+      )}
     </Group>
   );
 }
