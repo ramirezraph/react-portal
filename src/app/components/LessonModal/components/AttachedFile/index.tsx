8190c460bb92896cc2719c59408e5d233d46ae17
ramirezraph <ramirez.raph@yahoo.com>
2022-04-27 14:07:41 +0800

    pressable title for attached files

--- src/app/components/LessonModal/components/AttachedFile/index.tsx
+++ src/app/components/LessonModal/components/AttachedFile/index.tsx
@@ -1,4 +1,4 @@
-import { Group, ActionIcon, Text } from '@mantine/core';
+import { Group, ActionIcon, Text, Button } from '@mantine/core';
 import * as React from 'react';
 import { Pencil, At, Download, Trash, File } from 'tabler-icons-react';
 
@@ -14,12 +14,16 @@ export function AttachedFile(props: Prop) {
 
   return (
     <Group position="apart" className={`w-full ${className}`} noWrap>
-      <Group noWrap>
-        <File size={18} />
-        <Text size="sm" lineClamp={1}>
+      <Button
+        className="px-0 text-blue-700"
+        variant="subtle"
+        compact
+        leftIcon={<File size={18} />}
+      >
+        <Text weight={400} size="sm" lineClamp={1}>
           {name}
         </Text>
-      </Group>
+      </Button>
       <Group position="center" spacing={compact ? 'xs' : 'md'} noWrap>
         {!compact && (
           <>
